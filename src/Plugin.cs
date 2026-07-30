62cdba5d614071101fe7c62b3b0db68ee3f14716
CamOfFlage <github@slc.rister.net>
2026-07-29 21:52:14 -0400

    Initial Commit

---
+++ src/Plugin.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+
+namespace WorldlessLibs
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BasePlugin
+    {
+        public override void Load()
+        {
+            Log.LogMessage($"{PluginInfo.PLUGIN_NAME} loaded!");
+        }
+    }
+    
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "com.CamOfFlage.WorldlessLib";
+        public const string PLUGIN_NAME = "WorldlessLib";
+        public const string PLUGIN_VERSION = "0.0.1";
+    }
+}
