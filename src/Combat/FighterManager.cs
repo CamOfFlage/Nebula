f8b7d4b6417516ca53059cc4e07b0a2e47b54522
CamOfFlage <github@slc.rister.net>
2026-07-31 15:46:27 -0400

    Added FighterManager and the ability to retrieve a model from it

---
+++ src/Combat/FighterManager.cs
@@ -0,0 +1,22 @@
+﻿using Noname.Worldless.Combat;
+using UnityEngine;
+
+namespace WorldlessLibs.Combat;
+
+public class FighterManager
+{
+    public Fighter fighter { get; private set; }
+
+    public FighterManager(Fighter fighter)
+    {
+        this.fighter = fighter;
+    }
+
+    public GameObject getModel(String modelName)
+    {
+        GameObject fighterGameObject = fighter.gameObject;
+        GameObject models = fighterGameObject.transform.FindChild("Model").gameObject;
+        GameObject modelGameObject = models.transform.FindChild( modelName).gameObject;
+        return modelGameObject;
+    }
+}
